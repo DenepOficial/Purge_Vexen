75b4d0f32b65e5193c1e711a6372db58a917c060
DenepOficial <denepproducer@gmail.com>
2026-07-08 07:18:28 -0500

    Update database.py

--- database.py
+++ database.py
@@ -1,18 +1,20 @@
 import os
 import asyncpg
 
-
 DATABASE_URL = os.getenv("DATABASE_URL")
+BOT_NAME = os.getenv("BOT_NAME")
 
 
 if not DATABASE_URL:
-    raise Exception("DATABASE_URL no configurada en Railway")
+    raise Exception("DATABASE_URL no existe")
+
+
+if not BOT_NAME:
+    raise Exception("BOT_NAME no configurado")
 
 
 async def create_pool():
 
-    pool = await asyncpg.create_pool(
+    return await asyncpg.create_pool(
         DATABASE_URL
     )
-
-    return pool
